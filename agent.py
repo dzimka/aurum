81c287f442287c93c1f4044ad9b01efdec6615bf
Dima Kniazev <kniazev.dmitry@heb.com>
2025-02-27 09:01:46 -0600

    agent changes

--- agent.py
+++ agent.py
@@ -1,34 +1,38 @@
 from pprint import pprint
 from typing import Literal
 
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 
-prompt = ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate(
     [
         (
             "system",
-            "You are a helpful weather assistant. When responding about the weather, also give advice on what to weather in the city.",
+            "You are a helpful weather assistant.\
+                Always say the name of the user when responding. \
+                When responding about the weather, use the provided tool to get the numbers. \
+                Assume the temperature is in Farenheits. \
+                Also give advice on what to wear in the city.",
         ),
-        MessagesPlaceholder("messages"),
+        ("placeholder", "{messages}"),
     ]
 )
 
-model = ChatOllama(model="MFDoom/deepseek-r1-tool-calling:14b", temperature=0.8)
+model = ChatOllama(model="llama3.2:latest", temperature=0.8)
 
 
 @tool
 def get_weather(city: Literal["Round Rock", "Cedar Park"]):
-    """Returns the weather value for the specified city.
+    """Returns the temperature value for the specified city in Farenheits.
 
     Args:
         city (Literal["Round Rock", "Cedar Park"]): The city to get weather for.
             Must be either "Round Rock" or "Cedar Park".
 
     Returns:
-        str: The weather conditions for the specified city.
+        str: The weather conditions for the specified city in Farenheits.
     """
     resp = input(f"What's the weather in {city}?")
     return resp
@@ -38,7 +42,11 @@ tools = [get_weather]
 
 agent = create_react_agent(model, tools, prompt=prompt)
 
-inputs = {"messages": [("user", "what is the weather in round rock?")]}
+history = [("user", "My name is Dima")]
+query = "What is the weather in Round Rock?"
+history.append(("user", query))
+
+inputs = {"messages": history}
 
 resp = agent.stream(inputs, stream_mode="values")
 
